7e42bed510e9daf3c643123a54636fb0127ef9d8
Abhay Semwal <shbhtsemwal@gmail.com>
2022-09-23 02:20:57 +0530

    Add files via upload

---
+++ src/components/App.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import Navbar from "./Navbar";
+import Mustread from "./Mustread";
+function App() {
+  return (
+    <div>
+    <Navbar />
+    <Mustread />
+  </div>
+  );
+}
+
+export default App;
